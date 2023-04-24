116b86044c11fd0d5b082b99af9515c0b73796a2
Ashish Maharjan <ashish@explorug.net>
2023-04-24 21:25:17 +0545

    to ganache

--- context/constants.js
+++ context/constants.js
@@ -2,6 +2,6 @@
 
 import voting from "./Create.json";
 
-export const VotingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-// export const VotingAddress = "0x5c2fc29A6Eb40228cE2E4C3959C16D6B46e99109";
+// export const VotingAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+export const VotingAddress = "0x609D75de1Ad5aD85Fc48C4D077ca41a783102952";
 export const VotingAddressABI = voting.abi;
--- hardhat.config.js
+++ hardhat.config.js
@@ -4,12 +4,12 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.18",
   networks: {
-    // ganache: {
-    //   url: "http://localhost:7545",
-    //   chainId: 1337, // Replace with your desired chain ID
-    // },
-    hardhat: {
-      chainId: 1337,
+    ganache: {
+      url: "http://localhost:7545",
+      chainId: 1337, // Replace with your desired chain ID
     },
+    // hardhat: {
+    //   chainId: 1337,
+    // },
   },
 };
